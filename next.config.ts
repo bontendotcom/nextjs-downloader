50331e4d986ec9cfefa33268f2997d41526a37aa
bontendotcom <bontendotcom@gmail.com>
2025-09-19 09:49:49 +0900

    GitHub PagesのサブディレクトリにデプロイするためにbasePathを追加

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: '/nextjs-downloader', // GitHub Pagesのサブディレクトリにデプロイするために追加
   /* config options here */
 };
 
